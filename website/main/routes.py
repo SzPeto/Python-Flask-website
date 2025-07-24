d7845de7b15847faa1649983640c1fac4ab61b16
Peter <szpeto12@gmail.com>
2025-07-24 13:05:30 +0200

    Version v0.8.7.3

--- website/main/routes.py
+++ website/main/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
 
+from website.main.utils import *
+
 main_bp = Blueprint("main_bp", __name__)
 
 @main_bp.route("/")
